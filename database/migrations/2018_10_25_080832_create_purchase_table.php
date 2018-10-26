67355936a66892fa2a158a6d85c23939523b7bf7
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-26 12:04:03 +0900

    fix migrattion

--- database/migrations/2018_10_25_080832_create_purchase_table.php
+++ database/migrations/2018_10_25_080832_create_purchase_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePurchaseTable extends Migration
+class CreatePurchasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePurchaseTable extends Migration
      */
     public function up()
     {
-        Schema::create('purchase', function (Blueprint $table) {
+        Schema::create('purchases', function (Blueprint $table) {
             $table->increments('id');
             $table->string("user_id");
             $table->string("product_id");
@@ -28,6 +28,6 @@ class CreatePurchaseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('purchase');
+        Schema::dropIfExists('purchases');
     }
 }
