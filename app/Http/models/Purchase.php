faa5eb23e1d9c44d1d6cd68c48bab51c125eb774
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-26 12:02:32 +0900

    add fillable

--- app/Http/models/Purchase.php
+++ app/Http/models/Purchase.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
-    //
+    protected $fillable = array("usre_id", "product_id");
 }
--- app/Http/models/User.php
+++ app/Http/models/User.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    protected $fillable = array("name", "email", "phone_number", "address");
 }
