6c8248b0c00f0ed85495939682357f832b82067e
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-08 15:42:32 +0900

    add has many methods

--- app/Http/models/Purchase.php
+++ app/Http/models/Purchase.php
@@ -8,16 +8,13 @@ use App\Http\Models\User;
 
 class Purchase extends Model
 {
-    protected $fillable = array("usre_id", "product_id");
+    protected $fillable = array("user_id", "product_id");
 
-    public static function allTupled() {
-        $purchases = Purchase::all();
-        $iterator = 0;
-        $tuple = [];
-        foreach ($purchases as $purchase) {
-            $tuple[$iterator] = [User::where("id", $purchase->user_id)->first(), Product::where("id", $purchase->product_id)->first()];
-            $iterator++;
-        }
-        return $tuple;
+    public function user() {
+        return $this->belongsTo('App\Http\Models\User');
+    }
+
+    public function product() {
+        return $this->belongsTo('App\Http\Models\Product');
     }
 }
